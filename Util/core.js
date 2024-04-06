7a3e2e0739e50ece2e54875113a92c14ad186149
Mark Cooper <mdcoopir@gmail.com>
2024-04-06 10:33:44 -0700

    upgraded to triggersjs 0.0.7

--- Util/core.js
+++ Util/core.js
@@ -18,15 +18,15 @@ export const _autoInitTriggers = () => {
     window.document.querySelectorAll('[triggerInit]').forEach((el) => {
         const triggerName = el.hasAttribute('target') ? el.getAttribute('target'): 'default';
         const triggerEvent = el.hasAttribute('triggerEvent') ? el.getAttribute('triggerEvent'): 'click';
-        console.log(`triggerName = ${triggerName}`)
-        console.log(`triggerEvent = ${triggerEvent}`)
+        // console.log(`triggerName = ${triggerName}`)
+        // console.log(`triggerEvent = ${triggerEvent}`)
         const triggerCallbackSting = el.hasAttribute('triggerCallback') ? el.getAttribute('triggerCallback'): null;
         const triggerCallback = triggerCallbackSting ? window[triggerCallbackSting]: (window["triggerCallback"] == undefined) ? null: window['triggerCallback'];
         if(triggerCallback == undefined && triggerCallback != null) throw(new Error('triggerCalback is not defined'))
-        console.log(`triggerCallbackString = ${triggerCallbackSting}`)
-        console.log(`typeof triggerCallback = ${typeof triggerCallback}`);
-        console.log(`trigerCallbackString = ${triggerCallbackSting}`);
-        console.log(`\n\nwindow.document.getElementById('triggerScript').toggleClassesCallback = ${window.document.getElementById('triggerScript').toggleClassesCallback}`)
+        // console.log(`triggerCallbackString = ${triggerCallbackSting}`)
+        // console.log(`typeof triggerCallback = ${typeof triggerCallback}`);
+        // console.log(`trigerCallbackString = ${triggerCallbackSting}`);
+        // console.log(`\n\nwindow.document.getElementById('triggerScript').toggleClassesCallback = ${window.document.getElementById('triggerScript').toggleClassesCallback}`)
         initTriggers(triggerName, triggerEvent, triggerCallback, window.document);
     
     })    
